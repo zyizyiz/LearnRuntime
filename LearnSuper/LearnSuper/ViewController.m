a5c6085c860bd000144a2a8ebad2f2593368cc82
zyizyiz <1109580034@qq.com>
2019-01-29 10:22:36 +0800

    isKindOfClass isMemberOfClass

--- LearnSuper/LearnSuper/ViewController.m
+++ LearnSuper/LearnSuper/ViewController.m
@@ -29,6 +29,16 @@
     // my name is <ViewController: 0x7f9030f07b40>
     [(__bridge id)obj print];
     
+    // 1 0 0 0
+    // isKindOfClass : 代表是否为目标类或者其的子类  区分类对象和实例对象
+    // isMemberOfClass : 代表是否为目标类  区分类对象和实例对象
+    // isKindOfClass:[NSObject class]  永远为True  因为基类的元类的superclass是基类
+    // 类对象调用判断的是元类  实例对象调用判断的是类对象
+    NSLog(@"%d",[[People class] isKindOfClass:[NSObject class]]);
+    NSLog(@"%d",[[People class] isMemberOfClass:[NSObject class]]);
+    NSLog(@"%d",[[People class] isKindOfClass:[People class]]);
+    NSLog(@"%d",[[People class] isMemberOfClass:[People class]]);
+
 }
 
 
